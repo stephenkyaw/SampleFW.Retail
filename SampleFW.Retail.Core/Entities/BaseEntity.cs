d7a4fb1b956747e5d97b92964a883bd1c3c3fb87
Kyaw Myo Aung <info.kyawmyo@gmail.com>
2020-01-15 22:39:22 +0630

    Add Class Lib Project

---
+++ SampleFW.Retail.Core/Entities/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFW.Retail.Core.Entities
+{
+    public class BaseEntity
+    {
+        public string CreatedUserID { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string UpdatedUserID { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
