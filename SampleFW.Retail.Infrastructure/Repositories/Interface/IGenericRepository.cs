05d9f37e91f071cda4eb8cdd103f1b2d969cb3ae
Kyaw Myo Aung <info.kyawmyo@gmail.com>
2020-01-16 00:01:48 +0630

    install ef

---
+++ SampleFW.Retail.Infrastructure/Repositories/Interface/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace SampleFW.Retail.Infrastructure
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetByAll();
+
+        IEnumerable<TEntity> GetByAll(Expression<Func<TEntity,bool>> expression);
+
+        TEntity GetByID(object Id);
+
+        void Insert(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Remove(TEntity entity);
+
+    }
+}
